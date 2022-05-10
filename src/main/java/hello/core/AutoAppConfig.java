bf2a867d57cebc2f0a44b728f8b76391997d18a5
kiipo0623 <61893530+kiipo0623@users.noreply.github.com>
2022-05-10 15:17:37 +0900

    [0510] dependency injection - autowire

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -11,9 +11,5 @@ import org.springframework.context.annotation.FilterType;
 @ComponentScan(
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
-public class AutoAppConfig {
-    @Bean(name = "memoryMemberRepository")
-    MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
+public class AutoAppConfig{
 }
